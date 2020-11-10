35d78bf552b5ca71e9778475ef3757a593f22469
Md. Ahosan Hossain Sijan <sijanahosan@gmail.com>
2020-11-10 13:42:02 +0600

    Update app.js

--- Lab Performance/app.js
+++ Lab Performance/app.js
@@ -6,7 +6,8 @@ const cookieParser 		= require('cookie-parser');
 
 const login				= require('./controllers/login');
 const logout			= require('./controllers/logout');
-const home				= require('./controllers/home');
+const ahome				= require('./controllers/ahome');
+const ehome				= require('./controllers/ehome');
 const user				= require('./controllers/user');
 const app				= express();
 const port				= 3000;
@@ -16,14 +17,15 @@ app.set('view engine', 'ejs');
 
 
 //middleware
-app.use('/abc', express.static('assets'))
+//app.use('/abc', express.static('assets'))
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(exSession({secret: 'secret value', saveUninitialized: true, resave: false}));
 
 
 app.use('/login', login);
-app.use('/home', home);
+app.use('/ahome', ahome);
+app.use('/ehome', ehome);
 app.use('/logout', logout);
 app.use('/user', user);
 
